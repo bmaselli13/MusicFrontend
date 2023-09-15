e53b613f8f1cda18ac1782efc80d2f7fe6de2ab0
bmaselli13 <brian.maselli@gmail.com>
2023-09-15 15:31:55 -0700

    Finished project

--- src/App.jsx
+++ src/App.jsx
@@ -7,82 +7,100 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
+  // State to hold all songs fetched from the API
   const [songs, setSongs] = useState([]);
+
+  // State to hold filtered songs based on search
   const [filteredSongs, setFilteredSongs] = useState([]);
+
+  // State to hold search term entered by the user
   const [searchTerm, setSearchTerm] = useState("");
 
+  // useEffect hook to fetch songs from the API when the component mounts
   useEffect(() => {
-    // Fetch songs from API
+    // Making GET request to fetch all songs
     axios
       .get("https://localhost:7196/api/Songs")
       .then((response) => {
+        // Updating the songs state with the fetched data
         setSongs(response.data);
+        // Initially, all fetched songs are the filtered songs
         setFilteredSongs(response.data);
       })
       .catch((error) => {
+        // Handle and log any errors
         console.error("Error fetching songs:", error);
       });
   }, []);
 
+  // useEffect hook to update filteredSongs based on search term
   useEffect(() => {
-    const results = songs.filter(
-      (song) =>
-        song.title.toLowerCase().includes(searchTerm) ||
-        song.album.toLowerCase().includes(searchTerm) ||
-        song.artist.toLowerCase().includes(searchTerm) ||
-        song.genre.toLowerCase().includes(searchTerm) ||
-        song.releaseDate.toLowerCase().includes(searchTerm)
+    // Filter the songs based on search term
+    const results = songs.filter((song) =>
+      song.title.toLowerCase().includes(searchTerm.toLowerCase())
     );
+    // Update the state with filtered songs
     setFilteredSongs(results);
-  }, [searchTerm, songs]);
+  }, [searchTerm, songs]); // Re-run when searchTerm or songs change
 
+  // Function to add a new song
   const addSong = (newSong) => {
+    // Making POST request to add the song
     axios
       .post("https://localhost:7196/api/Songs", newSong)
       .then((response) => {
+        // Update the songs with the new song
         setSongs([...songs, response.data]);
       })
+      // Handle and log any errors
       .catch((error) => {
         console.error("Error adding song:", error);
       });
   };
 
+  // Function to delete a song
   const deleteSong = (songId) => {
+    // Making DELETE request to delete the song
     axios
       .delete(`https://localhost:7196/api/Songs/${songId}`)
       .then(() => {
-        setSongs((prevSongs) => prevSongs.filter((song) => song.id !== songId));
+        // Filter out the deleted song
+        setSongs(songs.filter((song) => song.id !== songId));
       })
+      // Handle and log any errors
       .catch((error) => {
         console.error("Error deleting song:", error);
       });
   };
 
+  // Function to update an existing song
   const updateSong = (updatedSong) => {
+    // PUT request to update the song
     axios
       .put(`https://localhost:7196/api/Songs/${updatedSong.id}`, updatedSong)
       .then((response) => {
-        setSongs((prevSongs) =>
-          prevSongs.map((song) =>
-            song.id === updatedSong.id ? updatedSong : song
-          )
+        // Find and update the song
+        setSongs(
+          songs.map((song) => (song.id === updatedSong.id ? updatedSong : song))
         );
       })
+      // Handle and log any errors
       .catch((error) => {
         console.error("Error updating song:", error);
       });
   };
 
+  // Render the components
   return (
     <div className="app">
       <Header />
       <SearchBar setSearchTerm={setSearchTerm} />
+      <AddSongForm addSong={addSong} />
       <SongList
         songs={filteredSongs}
         onDelete={deleteSong}
         onEdit={updateSong}
-      />{" "}
-      <AddSongForm addSong={addSong} />
+      />
     </div>
   );
 }
--- src/components/SongList/SongList.jsx
+++ src/components/SongList/SongList.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SongList.css";
 
 function SongList({ songs, onDelete, onEdit }) {
+  const [editingSongId, setEditingSongId] = useState(null);
+  const [editedSong, setEditedSong] = useState({});
+
+  const startEditing = (song) => {
+    setEditingSongId(song.id);
+    setEditedSong(song);
+  };
+
+  const handleInputChange = (event, field) => {
+    setEditedSong({
+      ...editedSong,
+      [field]: event.target.value,
+    });
+  };
+
+  const saveChanges = () => {
+    onEdit(editedSong);
+    setEditingSongId(null);
+  };
+
+  const cancelEditing = () => {
+    setEditingSongId(null);
+  };
+
   return (
     <table>
       <thead>
@@ -17,15 +41,57 @@ function SongList({ songs, onDelete, onEdit }) {
       <tbody>
         {songs.map((song) => (
           <tr key={song.id}>
-            <td>{song.title}</td>
-            <td>{song.artist}</td>
-            <td>{song.album}</td>
-            <td>{song.releaseDate.split("T")[0]}</td>
-            <td>{song.genre}</td>
-            <td>
-              <button onClick={() => onEdit(song)}>Edit</button>
-              <button onClick={() => onDelete(song.id)}>Delete</button>
-            </td>
+            {editingSongId === song.id ? (
+              <>
+                <td>
+                  <input
+                    value={editedSong.title || ""}
+                    onChange={(e) => handleInputChange(e, "title")}
+                  />
+                </td>
+                <td>
+                  <input
+                    value={editedSong.artist || ""}
+                    onChange={(e) => handleInputChange(e, "artist")}
+                  />
+                </td>
+                <td>
+                  <input
+                    value={editedSong.album || ""}
+                    onChange={(e) => handleInputChange(e, "album")}
+                  />
+                </td>
+                <td>
+                  <input
+                    type="date"
+                    value={editedSong.releaseDate.split("T")[0] || ""}
+                    onChange={(e) => handleInputChange(e, "releaseDate")}
+                  />
+                </td>
+                <td>
+                  <input
+                    value={editedSong.genre || ""}
+                    onChange={(e) => handleInputChange(e, "genre")}
+                  />
+                </td>
+                <td>
+                  <button onClick={saveChanges}>Save</button>
+                  <button onClick={cancelEditing}>Cancel</button>
+                </td>
+              </>
+            ) : (
+              <>
+                <td>{song.title}</td>
+                <td>{song.artist}</td>
+                <td>{song.album}</td>
+                <td>{song.releaseDate.split("T")[0]}</td>
+                <td>{song.genre}</td>
+                <td>
+                  <button onClick={() => startEditing(song)}>Edit</button>
+                  <button onClick={() => onDelete(song.id)}>Delete</button>
+                </td>
+              </>
+            )}
           </tr>
         ))}
       </tbody>
